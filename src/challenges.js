3f60a6e4f48ef0e02201fc5978e3dd19c544f826
Lianny Coringa <lianny_costa@hotmail.com>
2023-04-05 15:40:31 -0300

    Requisito 6 feito

--- src/challenges.js
+++ src/challenges.js
@@ -17,9 +17,40 @@ const concatName = (array) => `${array[array.length - 1]}, ${array[0]}`;
 // Desafio 4 - Crie a função footballPoints
 const footballPoints = (wins, ties) => wins * 3 + ties;
 // Desafio 5 - Crie a função highestCount
-
+// const highestNumber = (array) => {
+//   let number = array[0];
+//   for (let index = 1; index < array.length; index += 1) {
+//       if ((array[index]) > (number)) {
+//           number = array[index];
+//       };
+//       return number;
+//   };
+// };
+// const highestCount = (array) => {
+//   let highest = highestNumber(array);
+//   let quantity = 0;
+//   for (let numeroRepetido of array) {
+//       if (highest === numeroRepetido) {
+//           quantity += 1;
+//       }
+//   } return quantity;
+// }
 // Desafio 6 - Crie as funções calcTriangleArea, calcRectangleArea e calcAllAreas
-
+const calcTriangleArea = (base, height) => base * height / 2;
+const calcRectangleArea = (base, height) => base * height;
+const calcAllAreas = (base, height, form) => {
+    let calcTriangle = calcTriangleArea(base, height);
+    let calcRectangle = calcRectangleArea(base, height);
+    let message = '';
+    if (form === 'triângulo') {
+        message = `O valor da área do ${form} é de: ${calcTriangle}`;
+    } else if (form === 'retângulo') {
+        message = `O valor da área do ${form} é de: ${calcRectangle}`;
+    } else {
+        message = `Não foi possível fazer o cálculo, insira uma forma geométrica válida`;
+    }
+    return message;
+}
 // Desafio 7 - Crie a função catAndMouse
 
 // Desafio 8 - Crie a função fizzBuzz
