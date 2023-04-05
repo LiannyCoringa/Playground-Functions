4d18b0c058c361b97847afab49a5cb0eaff23f1c
Lianny Coringa <lianny_costa@hotmail.com>
2023-04-05 15:14:38 -0300

    Requisito 4

--- src/challenges.js
+++ src/challenges.js
@@ -15,7 +15,7 @@ const splitSentence = (frase) => {
 // Desafio 3 - Crie a função concatName
 const concatName = (array) => `${array[array.length - 1]}, ${array[0]}`;
 // Desafio 4 - Crie a função footballPoints
-
+const footballPoints = (wins, ties) => wins * 3 + ties;
 // Desafio 5 - Crie a função highestCount
 
 // Desafio 6 - Crie as funções calcTriangleArea, calcRectangleArea e calcAllAreas
